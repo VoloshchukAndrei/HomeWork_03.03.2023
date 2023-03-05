7168ed8bbecf51624f5524f7194faed0cae56991
Andrei Voloshchuk <troubleshooter.andrei@gmail.com>
2023-03-05 15:54:53 +0500

    Task047 - массив случайных вещественных числел

---
+++ Task047/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+double[,] array = GetArray(rows, columns);
+
+PrintArray(array);
+
+double[,] GetArray(int M, int N)
+{
+    double[,] result = new double[M, N];
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            result[i, j] = new Random().NextDouble()*(10 - (-9)) + (-9);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] N)
+{
+    for (int i = 0; i < N.GetLength(0); i++)
+    {
+        for (int j = 0; j < N.GetLength(1); j++)
+        {
+            Console.Write($"{N[i,j]: 0.0} ");
+        }
+        Console.WriteLine();
+    }
+}
